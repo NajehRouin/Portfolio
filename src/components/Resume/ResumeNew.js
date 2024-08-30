759e919d3092643073cadc48139376f95f6648ba
NajehRouin <rouin.najeh71@gmail.com>
2024-08-30 02:28:22 +0100

    up my cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_Rouin_Najeh.pdf";
+import pdf from "../../../src/Assets/CV_Rouin_Najeh.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
